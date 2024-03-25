969bc5f3704e3fb3a5ed4025f53009cbef45ae03
Engrimtiazahmed <160152394+Engrimtiazahmed@users.noreply.github.com>
2024-03-25 12:02:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+// // Day 2 Q No 1 
+// let personality: string = `"Hazrat Ali Ibn Abi Talib AS"`;
+// let qoute: string = `"Do not let your difficulties fill you with anxiety, after all it is only in the darkest nights that stars shine more brightly”`;
+// console.log(`${qoute} said by: ${personality}` );
+// // Day 2 Q No 2
+// let famous_person: string = "Hazrat Ali Ibn Abi Talib AS";
+// let message: string = `${famous_person} said by: "Do not let your difficulties fill you with anxiety, after all it is only in the darkest nights that stars shine more brightly”`;
+// console.log(`${qoute} said by: ${personality}` );
+// // Day 2 Q No 3
+let name1 = "\t\n \t Imtiaz Ahmed \t\n";
+console.log(name1);
+// // Day 3 Q No 1
+// console.log(6+2); //Addition
+// console.log(9-1); //Substraction
+// console.log(4*2); //Multiplication
+// console.log(24/3); //Division
+// // Day 3 Q No 9
+// let favourateNumber:number = 12;
+// console.log(`My favourate number is ${favourateNumber}`,);
+// // Day 3 Q No 10
+// let myName:string = ("Imtiaz Ahmed") //storing name in variable
+// console.log(`print: ${myName} will show in terminal`);
+// // Day 3 Q No 11
+// let names:string[] = ["Iqbal", "Shahid", "Hashim", "shamil"];
+// for (let i=0; i<names.length; i++){console.log(names[i]);}
+// // Day 3 Q No 12
+// let names:string[] = ["Iqbal", "Shahid", "Hashim", "shamil"];
+// for (let name of names) {console.log (`Hello ${name}, would you line to learn some typescript today?\n`);}
---
+++ index.ts
@@ -0,0 +1,81 @@
+// Day 1 Q No 2
+// let name1:string = "Imtiaz Ahmed";
+// let sent = "Would you like to learn typescript today?"
+// console.log(name1, "would you like to learn typescript today?");
+
+
+// //Day 1 Q No 3
+
+// let name1: string = "Imtiaz Ahmed";
+// // Uppercase
+// console.log(name1.toUpperCase());
+// // Lowercase
+// console.log(name1.toLowerCase());
+// //  First letter capital
+// console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase());
+
+
+// // Day 2 Q No 4 
+
+// let personality: string = `"Hazrat Ali Ibn Abi Talib AS"`;
+// let qoute: string = `"Do not let your difficulties fill you with anxiety, after all it is only in the darkest nights that stars shine more brightly”`;
+
+// console.log(`${qoute} said by: ${personality}` );
+
+// // Day 2 Q No 5
+
+// let famous_person: string = "Hazrat Ali Ibn Abi Talib AS";
+// let message: string = `${famous_person} said by: "Do not let your difficulties fill you with anxiety, after all it is only in the darkest nights that stars shine more brightly”`;
+
+// console.log(`${qoute} said by: ${personality}` );
+
+// // Day 2 Q No 6
+
+// let name1:string = "\t\n \t Imtiaz Ahmed \t\n";
+// console.log(name1);
+
+
+// // Day 3 Q No 7
+
+// console.log(6+2); //Addition
+// console.log(9-1); //Substraction
+// console.log(4*2); //Multiplication
+// console.log(24/3); //Division
+
+
+// // Day 3 Q No 9
+
+// let favourateNumber:number = 12;
+
+// console.log(`My favourate number is ${favourateNumber}`,);
+
+
+// // Day 3 Q No 10
+
+
+// let myName:string = ("Imtiaz Ahmed") //storing name in variable
+
+// console.log(`print: ${myName} will show in terminal`);
+
+
+
+
+
+// // Day 3 Q No 11
+
+// let names:string[] = ["Iqbal", "Shahid", "Hashim", "shamil"];
+// for (let i=0; i<names.length; i++){console.log(names[i]);}
+
+
+// // Day 3 Q No 12
+
+
+// let names:string[] = ["Iqbal", "Shahid", "Hashim", "shamil"];
+// for (let name of names) {console.log (`Hello ${name}, would you line to learn some typescript today?\n`);}
+
+
+
+
+
+
+
